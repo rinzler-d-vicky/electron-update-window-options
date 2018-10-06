6e30b2654c4b4b9d0010ea71b0bf379d18a4e625
Rinzler D. Vicky <rinzler.d.vicky@gmail.com>
2018-10-06 10:34:01 +0530

    [bugfix] ts-lints, jest

--- src/options.ts
+++ src/options.ts
@@ -9,57 +9,58 @@ export interface IOptions {
 }
 
 export type IMultiArgOptionMap = [
-  [string[], (window: BrowserWindow, ...args: any[]) => void]
+  string[],
+  (window: BrowserWindow, ...args: any[]) => void
 ];
 
 export const optionMap: IOptions = {
+  alwaysOnTop: (browserWindow: BrowserWindow, alwaysOnTop: boolean) =>
+    browserWindow.setAlwaysOnTop(alwaysOnTop),
+  autoHideMenuBar: (browserWindow: BrowserWindow, autoHideMenuBar: boolean) =>
+    browserWindow.setAutoHideCursor(autoHideMenuBar),
   center: (browserWindow: BrowserWindow, center: boolean) => {
     if (center) {
       browserWindow.center();
     }
   },
-  resizable: (browserWindow: BrowserWindow, resizable: boolean) =>
-    browserWindow.setResizable(resizable),
-  movable: (browserWindow: BrowserWindow, movable: boolean) =>
-    browserWindow.setMovable(movable),
-  minimizable: (browserWindow: BrowserWindow, minimizable: boolean) =>
-    browserWindow.setMinimizable(minimizable),
-  maximizable: (browserWindow: BrowserWindow, maximizable: boolean) =>
-    browserWindow.setMaximizable(maximizable),
   closable: (browserWindow: BrowserWindow, closable: boolean) =>
     browserWindow.setClosable(closable),
-  focusable: (browserWindow: BrowserWindow, focusable: boolean) =>
-    browserWindow.setFocusable(focusable),
-  alwaysOnTop: (browserWindow: BrowserWindow, alwaysOnTop: boolean) =>
-    browserWindow.setAlwaysOnTop(alwaysOnTop),
-  fullscreen: (browserWindow: BrowserWindow, fullscreen: boolean) =>
-    browserWindow.setFullScreen(fullscreen),
-  fullscreenable: (browserWindow: BrowserWindow, fullscreenable: boolean) =>
-    browserWindow.setFullScreenable(fullscreenable),
-  // On the docs but not on the typings
-  // https://github.com/electron/electron/blob/master/docs/api/browser-window.md#winsetsimplefullscreenflag-macos
-  // simpleFullscreen: (browserWindow: BrowserWindow, simpleFullscreen: boolean) =>
-  //   browserWindow.setSimpleFullScreen(simpleFullscreen),
-  skipTaskbar: (browserWindow: BrowserWindow, skipTaskbar: boolean) =>
-    browserWindow.setSkipTaskbar(skipTaskbar),
-  kiosk: (browserWindow: BrowserWindow, kiosk: boolean) =>
-    browserWindow.setKiosk(kiosk),
-  title: (browserWindow: BrowserWindow, title: string) =>
-    browserWindow.setTitle(title),
-  icon: (browserWindow: BrowserWindow, icon: NativeImage) =>
-    browserWindow.setIcon(icon),
-  show: (browserWindow: BrowserWindow, show: boolean) =>
-    show ? browserWindow.show() : browserWindow.hide(),
-  parent: (browserWindow: BrowserWindow, parent: BrowserWindow) =>
-    browserWindow.setParentWindow(parent),
   disableAutoHideCursor: (
     browserWindow: BrowserWindow,
     autoHideCursor: boolean
   ) => browserWindow.setAutoHideCursor(!autoHideCursor),
-  autoHideMenuBar: (browserWindow: BrowserWindow, autoHideMenuBar: boolean) =>
-    browserWindow.setAutoHideCursor(autoHideMenuBar),
+  focusable: (browserWindow: BrowserWindow, focusable: boolean) =>
+    browserWindow.setFocusable(focusable),
+  fullscreen: (browserWindow: BrowserWindow, fullscreen: boolean) =>
+    browserWindow.setFullScreen(fullscreen),
+  fullscreenable: (browserWindow: BrowserWindow, fullscreenable: boolean) =>
+    browserWindow.setFullScreenable(fullscreenable),
   hasShadow: (browserWindow: BrowserWindow, hasShadow: boolean) =>
     browserWindow.setHasShadow(hasShadow),
+  icon: (browserWindow: BrowserWindow, icon: NativeImage) =>
+    browserWindow.setIcon(icon),
+  kiosk: (browserWindow: BrowserWindow, kiosk: boolean) =>
+    browserWindow.setKiosk(kiosk),
+  maximizable: (browserWindow: BrowserWindow, maximizable: boolean) =>
+    browserWindow.setMaximizable(maximizable),
+  minimizable: (browserWindow: BrowserWindow, minimizable: boolean) =>
+    browserWindow.setMinimizable(minimizable),
+  movable: (browserWindow: BrowserWindow, movable: boolean) =>
+    browserWindow.setMovable(movable),
+  parent: (browserWindow: BrowserWindow, parent: BrowserWindow) =>
+    browserWindow.setParentWindow(parent),
+  resizable: (browserWindow: BrowserWindow, resizable: boolean) =>
+    browserWindow.setResizable(resizable),
+  // On the docs but not on the typings
+  // https://github.com/electron/electron/blob/master/docs/api/browser-window.md#winsetsimplefullscreenflag-macos
+  // simpleFullscreen: (browserWindow: BrowserWindow, simpleFullscreen: boolean) =>
+  //   browserWindow.setSimpleFullScreen(simpleFullscreen),
+  show: (browserWindow: BrowserWindow, show: boolean) =>
+    show ? browserWindow.show() : browserWindow.hide(),
+  skipTaskbar: (browserWindow: BrowserWindow, skipTaskbar: boolean) =>
+    browserWindow.setSkipTaskbar(skipTaskbar),
+  title: (browserWindow: BrowserWindow, title: string) =>
+    browserWindow.setTitle(title),
   // On the docs but not on the typings
   // https://github.com/electron/electron/blob/master/docs/api/browser-window.md#winsetopacityopacity-windows-macos
   // opacity: (browserWindow: BrowserWindow, opacity: number) =>
@@ -80,7 +81,7 @@ export const optionMap: IOptions = {
   ) => browserWindow.setVibrancy(vibrancy)
 };
 
-export const multiArgOptionMap: IMultiArgOptionMap = [
+export const multiArgOptionMap: IMultiArgOptionMap[] = [
   [
     ["width", "height"],
     (browserWindow: BrowserWindow, width: number, height: number) =>
